be52b469a8b6ccd2756c5bedaa77ce60466ed4f9
Ahmed Fouad <18531447+FoushWare@users.noreply.github.com>
2023-02-02 13:14:27 -0800

    update vite config
    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/pokemon-gallery/",
 	plugins: [react()],
-	// base for github pages
-	base: "/",
 });
